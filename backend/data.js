db87e59dcb9ca33a5d0eb5f0f134890a85ce4744
Mjeanbapti <marcjeanb0@gmail.com>
2021-05-13 15:50:42 -0400

    Initial commit

---
+++ backend/data.js
@@ -0,0 +1,70 @@
+import bcrypt from 'bcryptjs';
+const data = {
+    users: [
+        {
+            name:'Marc',
+            email: 'marcjeanb@template.com',
+            password: bcrypt.hashSync('123456', 8),
+            isAdmin: true,
+        },
+        {
+            name:'John',
+            email: 'john@template.com',
+            password: bcrypt.hashSync('123456', 8),
+            isAdmin: false,
+        }
+    ],
+    products:[
+        {
+            name:'Nike Slim T-Shirt',
+            category: "Shirts",
+            image: '/images/d1.jpg',
+            price: 120, countInStock: 10,
+            brand: 'Nike',
+            rating: 3,
+            numReviews: 10,
+            description: 'high quality products',
+        },
+        {
+            name:'Nike Shirt',
+            category: "Shirts",
+            image: '/images/d1.jpg',
+            price: 120, countInStock: 20,
+            brand: 'Nike',
+            rating: 2,
+            numReviews: 10,
+            description: 'high quality products',
+        },
+        {
+            name:'Nike Short',
+            category: "Shirts",
+            image: '/images/d1.jpg',
+            price: 120, countInStock: 30,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'high quality products',
+        },
+        {
+            name:'Adidas Slim Shirt',
+            category: "Shirts",
+            image: '/images/d1.jpg',
+            price: 120, countInStock: 25,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 10,
+            description: 'high quality products',
+        },
+        {
+            name:'Nike shoes',
+            category: "Shirts",
+            image: '/images/d1.jpg',
+            price: 120, countInStock:0,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality products',
+        },
+    ],
+};
+export default data;

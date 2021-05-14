06ad5a1db6018db639f71d7ec2fee685b9fe9cd2
Mjeanbapti <marcjeanb0@gmail.com>
2021-05-13 21:38:22 -0400

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,9 @@ app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/in
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
-});
+//app.use((err, req, res, next) => {
+//  res.status(500).send({ message: err.message });
+//});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
